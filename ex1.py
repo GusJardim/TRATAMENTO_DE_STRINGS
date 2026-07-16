2d5b20ad9ff84abb2f6a5bd8581716d5c4cb4710
GusJardim <ajardim.gustavo@gmail.com>
2026-07-16 18:14:47 -0300

    Primeira e Última Letra

---
+++ ex1.py
@@ -0,0 +1,8 @@
+#Peça ao usuário para digitar uma palavra. Mostre:
+
+#A primeira letra
+
+#A última letra
+
+palavra = input("Digite uma palavra: ")
+print(f"A primeira letra da palavra {palavra}: {palavra[0]}\n E a segunda letra e: {palavra[-1]} ")
