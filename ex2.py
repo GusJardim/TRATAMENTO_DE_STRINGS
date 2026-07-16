7584da6606ff18627c0b3c64288fa04c481e1354
GusJardim <ajardim.gustavo@gmail.com>
2026-07-16 18:28:40 -0300

    Pegando um trecho da frase

---
+++ ex2.py
@@ -0,0 +1,6 @@
+frase = input("Digite uma frase: ")
+inicio = int(input("Digite um numero para fatiar: "))
+final  = int(input("Digite um numero para fatiar: "))
+
+
+print(f"As letra fatiada de: {frase}\n primeira: {frase[inicio]}\n segunda: {frase[final]} ")
